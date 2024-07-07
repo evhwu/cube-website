a9918b4413de2b31feb9fcd54a70019a142725c7
Evan Wu <evan.h.wu@gmail.com>
2024-07-06 22:18:36 -0700

    formatting

--- xlsx_to_json.py
+++ xlsx_to_json.py
@@ -14,23 +14,18 @@ def xlsx_to_json():
         file_num, file_ext = os.path.splitext(f)
         file_name = os.path.join(save_path, f)
         
+        date_sheet = pd.read_excel(file_name,sheet_name="Date", header=None)    
         draft["draft_number"] = file_num
-        
-        date_sheet = pd.read_excel(file_name,sheet_name="Date", header=None)
-        
         draft["date"] = date_sheet.iloc[0,0]
         draft["patch"] = date_sheet.iloc[1,0]
         
         matches = []     
         match_sheet = pd.read_excel(file_name,sheet_name="Results", usecols=[0,1,2])
-        
         is_swap = False 
         lastrow = pd.Series()
-        
         def write_row(row):
             loser = row["Player 1"] if row["Winner"] == row["Player 2"] else row["Player 2"]
             matches.append({"winner" : row["Winner"], "loser" : loser})
-
         for index, row in match_sheet.iterrows():
             if index == 2:
                 if row["Player 1"] == lastrow["Winner"] or row["Player 2"] == lastrow["Winner"]:
@@ -56,17 +51,14 @@ def xlsx_to_json():
                                        "number" : i + 1,
                                        "cards" : packs[i*15:((i + 1) * 15)]})
         
-        
         rank_sheet = pd.read_excel(file_name,sheet_name="Results", usecols=[4], skiprows=[5,6])
         pick_order_sheet = pd.read_excel(file_name,sheet_name="Draft", skiprows=[16,32],
                                           usecols=[0,1,2,3], header=0)
         play_sheet = pd.read_excel(file_name,sheet_name="Play").dropna()
-        
         draft["players"] = []
         for index, row in rank_sheet.iterrows():
             draft["players"].append({"name": row["Ranking"],
                                      "place": index + 1})
-            
         for p in draft["players"]:
             for series_name, series in pick_order_sheet.items():
                 if series_name == p["name"]:
