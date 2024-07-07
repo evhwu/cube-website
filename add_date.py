c32c630c8d3e9763ef36647e1a6425ad6a571c00
Evan Wu <evan.h.wu@gmail.com>
2024-07-06 21:43:46 -0700

    added headings to pack parts of draft sheet

--- add_date.py
+++ add_date.py
@@ -4,24 +4,38 @@ import os
 BASE_PATH = os.getcwd()
 save_path = os.path.dirname(f"{BASE_PATH}/archive/")
 
-archive_files = os.listdir(save_path)
-patch = "9.0"
+def copy_headings():
+    archive_files = os.listdir(save_path)
+    for f in archive_files:
+        wb = load_workbook(os.path.join(save_path, f))
+        current_sheet = wb["Draft"]
+        
+        current_sheet["F1"] = current_sheet["A1"].value
+        current_sheet["G1"] = current_sheet["B1"].value
+        current_sheet["H1"] = current_sheet["C1"].value
+        current_sheet["I1"] = current_sheet["D1"].value
+        wb.save(os.path.join(save_path, f))
 
-for f in archive_files:
-    file_num, file_ext = os.path.splitext(f)
-    
-    wb = load_workbook(os.path.join(save_path, f))
-    wb.create_sheet(title="Date")
-    current_sheet = wb["Date"]
-    
-    print(f"{file_num}: ")
-    date = input("Insert Date: (ex. 11/20/2020): ")
-    patch_input = input(f"Insert Patch Number (ex. 2.1, 3.0) or Enter for last value ({patch}):  ")
-    if not patch_input == "":
-        patch = patch_input
+copy_headings()
 
-    current_sheet["A1"] = date
-    current_sheet["A2"] = patch   
-    
-    wb.save(os.path.join(save_path, f))
+def add_date():
+    archive_files = os.listdir(save_path)
+
+    patch = "9.0"
+    for f in archive_files:
+        file_num, file_ext = os.path.splitext(f)
+        
+        wb = load_workbook(os.path.join(save_path, f))
+        wb.create_sheet(title="Date")
+        current_sheet = wb["Date"]
+        
+        print(f"{file_num}: ")
+        date = input("Insert Date: (ex. 11/20/2020): ")
+        patch_input = input(f"Insert Patch Number (ex. 2.1, 3.0) or Enter for last value ({patch}):  ")
+        if not patch_input == "":
+            patch = patch_input
+
+        current_sheet["A1"] = date
+        current_sheet["A2"] = patch   
+        wb.save(os.path.join(save_path, f))
     
